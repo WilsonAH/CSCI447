baa66cbd7fbd5e6c83f1bc6da703ff58b40690da
lbsrussett <lbsrussett@gmail.com>
2017-09-29 14:01:46 -0600

    added links

--- src/RBFNetwork.java
+++ src/RBFNetwork.java
@@ -8,11 +8,18 @@ public class RBFNetork {
 		
 	}
 	
+	private void findSigma(double[] inputs) {
+
+	}
+	
 	public double getOutput(double inputs[]){
 		double[] outputs = inputs;
 		for(int loop = 0; loop < layers.length; loop++){
 			outputs = layers[loop].getOutputs(outputs);
 		}
 		return outputs[0];
+	}
+	private double getDistance(double[] x1, double[] x2) {
+
 	}
 }
