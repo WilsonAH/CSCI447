693c6510833488586388ec44d52388addf7016f7
lbsrussett <lbsrussett@gmail.com>
2017-10-20 11:08:20 -0600

    First commit of skeleton GA classes

---
+++ Evolution/src/evolution/Evolution.java
@@ -0,0 +1,8 @@
+package evolution;
+
+public class Evolution {
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Evolution/src/evolution/GeneticAlgorithm.java
@@ -0,0 +1,48 @@
+package evolution;
+
+public class GeneticAlgorithm {
+	private int genCounter;
+	
+	public GeneticAlgorithm() {
+		this.genCounter = 0;
+	}
+	public void intialize() {
+		
+	}
+	public void fitness() {
+		
+	}
+	
+	public void selectParents() {
+		
+	}
+	
+	public void mutate() {
+		
+	}
+	
+	public void crossOver() {
+		
+	}
+	
+	
+	public void offspringFitness() {
+		
+	}
+	
+	public void replacePopulation() {
+		
+	}
+	
+	public boolean terminate() {
+		if(genCounter == 10) {
+			return true;
+		}
+		else
+			return false;
+	}
+	public void result() {
+		
+	}
+	
+}
